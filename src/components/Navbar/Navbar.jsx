ba62d8b26231f8b6f8219670ed2db68aa937b083
sujaykundu777 <sujaykundu777@gmail.com>
2020-11-25 06:39:14 +0530

    added bit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useState } from 'react';
-import styles from '../../styles/Navbar.module.css';
+import styles from './Navbar.module.css';
 
 const Navbar = props => {
 
